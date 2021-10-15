cb33f80bd56adf1bd6b62bc198e059df4422cfc5
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-15 18:49:20 -0300

    feat: add index routes

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+import moviesRouter from './movies.routes';
+
+const routes = Router();
+
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/movies', moviesRouter);
+routes.use('/movies/top', moviesRouter);
+
+export default routes;
