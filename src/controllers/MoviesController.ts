df64d3f3ce1a3d2da78f9350069b1ac4a9c56675
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-15 18:47:11 -0300

    feat: add movies controller

---
+++ src/controllers/MoviesController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import CreateMovieService from '../services/CreateMovieServices';
+import ListMovieService from '../services/ListMovieService';
+
+export default class MoviesController {
+  public async show(request: Request, response: Response) {
+    const listMovies = new ListMovieService();
+
+    const movies = await listMovies.listAll();
+
+    return response.json(movies);
+  }
+
+  public async findById(request: Request, response: Response) {
+    const movies = new ListMovieService();
+    const { id } = request.params;
+
+    const movie = await movies.findById({ id });
+
+    return response.json(movie);
+  }
+
+  public async findTopList(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const movies = new ListMovieService();
+
+    const movie = await movies.findTop();
+    console.log(movies);
+
+    return response.json(movie);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const createMovie = new CreateMovieService();
+    const { title, director, category, year, review, sinopse } = request.body;
+
+    const movie = await createMovie.execute({
+      title,
+      director,
+      category,
+      sinopse,
+      year,
+      review,
+    });
+
+    return response.json(movie);
+  }
+}
