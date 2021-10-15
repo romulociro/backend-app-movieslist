d562b8cdd8d3fe4377a1368494c7b2d898ac9b9d
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-15 10:50:27 -0300

    feat: add user routes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { username, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      username,
+      password,
+    });
+
+    return response.json({ username: user.username, date: user.date });
+  } catch (err: any) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
