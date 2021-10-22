2acf203942bbf9c091d82d523dc0badf6ae0d943
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-22 18:10:16 -0300

    fix: config database

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 class Db {
   private DB_URL =
-    'mongodb://adminrc:dnrtuff123@localhost:27017/movieslist?authSource=admin';
+    'mongodb://admin:movielist123@localhost:27017/movieslistdb?authSource=admin';
 
   createConnection() {
     mongoose.connect(this.DB_URL);
