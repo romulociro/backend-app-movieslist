a7d2f95a1796690733363ca34aa0970177fe800f
Romulo Ciro <romulo.ciro@hotmail.com>
2021-10-22 17:28:33 -0300

    feat: add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
 import Db from './database';
 import ensureErrors from './middlewares/ensureErrors';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
@@ -13,8 +15,10 @@ database.createConnection();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(routes);
 app.use(ensureErrors);
 
 app.listen(3333, () => {
